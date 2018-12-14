78b2222ea6e833f64bbd6c003c795b52cef92e7f
zhengbw <1317812522@qq.com>
2018-12-14 23:44:03 +0800

    添加搜索模块静态页面

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,33 @@
 $(function () {
-    $("body").niceScroll({ cursorwidth: 8 });
+    // loading start
+    NProgress.start();
+
+    // set scrollbar
+    $('body').niceScroll({ cursorwidth: 8, cursorcolor: '#778886' });
+
+    // search
+    $('#search-input').on('click', function () {
+        $('.js-search-panel').show();
+    });
+
+    $('.js-close').on('click', function () {
+        $('.js-search-panel').hide();
+        $('.js-search-input').val('');
+    });
+
+    // set search result container scrollbar
+    $('.js-search-result').niceScroll({
+        cursorwidth: 8,
+        background: '#eee',
+        cursorcolor: '#78887d',
+        railpadding: { top: 0, right: 0, left: 0, bottom: 0 },
+        autohidemode: false,
+        cursorborderradius: 0,
+        cursorborder: 0
+    });
+
+    // loading done
+    setTimeout(function () {
+        NProgress.done();
+    }, 3000);
 });
